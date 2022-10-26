b9c8ff38f3e74f44cc00a8bc6e901406d96cedf7
Maxwell Peterson <mpeterson94@me.com>
2022-10-26 12:11:52 -0700

    finsihed functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,25 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var front = 0;
+  var total = 0;
 
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    storage[total] = value;
+    total++;
   };
 
   someInstance.dequeue = function() {
+    if (front < total) {
+      var memo = storage[front];
+      front++;
+      return memo;
+    }
   };
 
   someInstance.size = function() {
+    return total - front;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var top = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[top] = value;
+    top++;
   };
 
   someInstance.pop = function() {
+    if (top > 0) {
+      var memo = storage[top-1];
+      delete storage[top-1];
+      top--;
+      return memo;
+    }
   };
 
   someInstance.size = function() {
+    return top;
   };
 
   return someInstance;
