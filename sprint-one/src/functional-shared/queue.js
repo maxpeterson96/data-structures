e6e07483e48b5f8a3274f8219a67066e274a663f
Maxwell Peterson <mpeterson94@me.com>
2022-10-26 14:12:01 -0700

    done with functional shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {
+    storage: {},
+    front: 0,
+    total: 0,
+    enqueue: queueMethods.enqueue,
+    dequeue: queueMethods.dequeue,
+    size: queueMethods.size
+  }
+  return instance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this.storage[this.total] = value;
+  this.total++;
+};
+
+queueMethods.dequeue = function() {
+  if (this.front < this.total) {
+    var memo = this.storage[this.front];
+    this.front++;
+    return memo;
+  }
+};
+
+queueMethods.size = function() {
+  return this.total - this.front;
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,41 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {
+    storage: {},
+    top: 0,
+    push: stackMethods.push,
+    pop: stackMethods.pop,
+    size: stackMethods.size
+  }
+  return instance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.top] = value;
+  this.top++;
+};
 
+stackMethods.pop = function() {
+  if (this.top > 0) {
+    var memo = this.storage[this.top-1];
+    delete this.storage[this.top-1];
+    this.top--;
+    return memo;
+  }
+};
+
+stackMethods.size = function() {
+  return this.top;
+};
+
+
+// var Stack = function() {
+//   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+//   // but try not not reference your old code in writing the new style.
+//   var someInstance = {};
+// };
+
+// var stackMethods = {};
