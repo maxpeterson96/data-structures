612aeaf3c6543e6051bc9c064877eaab4710c1c9
Maxwell Peterson <mpeterson94@me.com>
2022-10-27 17:35:58 -0700

    finished up to hashTables.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,28 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // put v in this storage at index
+  this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // return storage at index
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // iterate over storage using each, pass in function to remove index
+  this._storage.set(index, undefined);
+
+  // yessir cheers bruv
+  // i think maybe we can handle collisions in insert
+  // before setting, check if undefined, set to index, else recalculate index until undefiend is found?
+  // im not sure, bc like i said before when i was running the hash it was alwaus returning 0
+  // we got this
+  // great job today ill upload to git have a goodn night!
+
 };
 
 
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -22,5 +22,3 @@ setPrototype.remove = function(item) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
